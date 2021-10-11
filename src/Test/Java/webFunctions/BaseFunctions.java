4f41d36074cfeb98db8acbe8c73f22a5e9ce9927
Toby Whitehead <toby.whitehead@QUALITEST.LOCAL>
2021-10-11 13:10:36 +0100

    added implicit timeouts and increased resuability of By parameters

--- src/Test/Java/webFunctions/BaseFunctions.java
+++ src/Test/Java/webFunctions/BaseFunctions.java
@@ -10,6 +10,10 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseFunctions {
     private WebDriver driver;
+    By wishListTable = By.xpath("//table/tbody/tr");
+    By cartTable = By.cssSelector(".cart");
+    By productCost = By.cssSelector(".product-price bdi");
+
 
     public void setUp() {
         WebDriverManager.chromedriver().setup();
@@ -20,22 +24,24 @@ public class BaseFunctions {
 
     public void navigateTo(String url) {
         driver.get(url);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     public int getWishlistTableSize() {
-        return driver.findElements(By.xpath("//table/tbody/tr")).size();
+        return driver.findElements(wishListTable).size();
     }
 
     public int getCartSize() {
-        return driver.findElements(By.cssSelector(".cart")).size();
+        return driver.findElements(cartTable).size();
     }
 
     public double getCartProductCost() {
-        return Double.parseDouble(driver.findElement(By.cssSelector(".product-price bdi")).getText().substring(1));
+        return Double.parseDouble(driver.findElement(productCost).getText().substring(1));
     }
 
     public double getItemPrice(String id) {
-        String stringPrice = driver.findElement(By.cssSelector("#yith-wcwl-row-" + id + " ins bdi")).getText();
+        By cartItemPrice = By.cssSelector("#yith-wcwl-row-" + id + " ins bdi");
+        String stringPrice = driver.findElement(cartItemPrice).getText();
         return Double.parseDouble(stringPrice.substring(1));
     }
 
